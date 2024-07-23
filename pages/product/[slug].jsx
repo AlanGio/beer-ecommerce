98cf85bda4e510e065e3c31f7b2ba64e1aba2bf8
Alan <alan.gio@gmail.com>
2024-07-22 22:20:42 -0300

    Adding time interval to check stock and price

--- pages/product/[slug].jsx
+++ pages/product/[slug].jsx
@@ -18,10 +18,6 @@ import { getProducts, getSku } from "../../setup/connection";
 const formatter = new Intl.NumberFormat("en-US", {
   style: "currency",
   currency: "USD",
-
-  // These options are needed to round to whole numbers if that's what you want.
-  //minimumFractionDigits: 0, // (this suffices for whole numbers, but will print 2500.10 as $2,500.1)
-  //maximumFractionDigits: 0, // (causes 2500.99 to be printed as $2,501)
 });
 
 export default function Home() {
@@ -29,13 +25,6 @@ export default function Home() {
   const [stock, setStock] = useState(null);
   const [size, setSize] = useState(null);
   const itemId = router.query.slug && router.query.slug.split("-")[0];
-
-  useEffect(() => {
-    if (router.isReady) {
-      // Code using query
-      console.log(router.query);
-    }
-  }, [router.isReady]);
 
   const [{ data: product, loading, error }, refetch] = getProducts(
     `/?id=${itemId}`,
@@ -63,17 +52,14 @@ export default function Home() {
     }
   }, [product]);
 
-  if (loading || loadingSku) {
-    return (
-      <CircularProgress
-        sx={{
-          position: "absolute",
-          top: "calc(50% - 25px)",
-          left: "calc(50% - 25px)",
-        }}
-      />
-    );
-  }
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      //assign interval to a variable to clear it.
+      size && refetchSku();
+    }, 5000);
+
+    return () => clearInterval(intervalId); //This is important
+  }, [size]);
 
   if (error || errorSku) {
     return (
@@ -90,75 +76,81 @@ export default function Home() {
       <Container sx={{ minWidth: 420 }}>
         <Header leftButton="back" rightButton="more" title="Detail" />
 
-        {product && (
-          <>
-            <Box
-              sx={{
-                width: "100%",
-                height: 240,
-                textAlign: "center",
-                "& img": { width: "fit-content", height: "90%" },
-              }}
-            >
-              <img
-                src={`/images/detail${
-                  product.image.substr(0, product.image.lastIndexOf(".")) +
-                  ".png"
-                }`}
-                alt={product.brand}
-              />
-            </Box>
+        <>
+          <Box
+            sx={{
+              width: "100%",
+              height: 240,
+              textAlign: "center",
+              "& img": { width: "fit-content", height: "90%" },
+            }}
+          >
+            <img
+              src={`/images/detail${
+                product?.image.substr(0, product?.image.lastIndexOf(".")) +
+                ".png"
+              }`}
+              alt={product?.brand}
+            />
+          </Box>
 
-            <Box
-              sx={{ display: "flex", flexDirection: "column", gap: 2, mb: 14 }}
-            >
-              {stock && (
-                <>
-                  <Box
-                    sx={{ display: "flex", justifyContent: "space-between" }}
-                  >
-                    <Typography variant="h2">{product.brand}</Typography>
-                    <Typography variant="h2" sx={{ color: "primary.main" }}>
-                      {formatter.format(stock.price)}
-                    </Typography>
-                  </Box>
-                  <Typography variant="subtitle1">
-                    Origin: {product.origin} | Stock: {stock.stock}
+          <Box
+            sx={{ display: "flex", flexDirection: "column", gap: 2, mb: 14 }}
+          >
+            {stock && (
+              <>
+                <Box sx={{ display: "flex", justifyContent: "space-between" }}>
+                  <Typography variant="h2">{product.brand}</Typography>
+                  <Typography variant="h2" sx={{ color: "primary.main" }}>
+                    {formatter.format(stock.price)}
                   </Typography>
-                </>
-              )}
+                </Box>
+                <Typography variant="subtitle1">
+                  Origin: {product?.origin} | Stock: {stock.stock}
+                </Typography>
+              </>
+            )}
 
-              <Typography variant="h3" sx={{ mt: 2 }}>
-                Description
-              </Typography>
-              <Typography variant="subtitle1">{product.information}</Typography>
+            <Typography variant="h3" sx={{ mt: 2 }}>
+              Description
+            </Typography>
+            <Typography variant="subtitle1">{product?.information}</Typography>
 
-              <Typography variant="h3" sx={{ mt: 2 }}>
-                Size
-              </Typography>
+            <Typography variant="h3" sx={{ mt: 2 }}>
+              Size
+            </Typography>
 
-              {stock && (
-                <>
-                  <Box sx={{ display: "flex", gap: 2 }}>
-                    {product.skus.map((sku) => (
-                      <Chip
-                        label={sku.name}
-                        variant="outlined"
-                        color={size === sku.code ? "primary" : "default"}
-                        onClick={() => setSize(sku.code)}
-                      />
-                    ))}
-                  </Box>
-                  <Footer
-                    brand={product.brand}
-                    sku={size}
-                    price={formatter.format(stock.price)}
-                  />
-                </>
-              )}
-            </Box>
-          </>
-        )}
+            {stock && (
+              <>
+                <Box sx={{ display: "flex", gap: 2 }}>
+                  {product.skus.map((sku) => (
+                    <Chip
+                      label={sku.name}
+                      variant="outlined"
+                      color={size === sku.code ? "primary" : "default"}
+                      onClick={() => setSize(sku.code)}
+                    />
+                  ))}
+                </Box>
+                <Footer
+                  brand={product?.brand}
+                  sku={size}
+                  price={formatter.format(stock.price)}
+                />
+              </>
+            )}
+
+            {(loading || loadingSku) && (
+              <CircularProgress
+                sx={{
+                  position: "absolute",
+                  bottom: 80,
+                  left: "calc(50% - 25px)",
+                }}
+              />
+            )}
+          </Box>
+        </>
       </Container>
     </ThemeProvider>
   );
